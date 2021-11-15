a0d1db40036b62f8d542b879535fc48821743cac
i728 <shunsei728@icloud.com>
2021-11-15 15:30:54 +0900

    record

---
+++ user/main.js
@@ -0,0 +1,18 @@
+var app5 = new Vue({
+    el:'#app-5',
+    data:{
+        message:'Hello Vue.js!'
+    },
+    methods:{
+        reverseMessage:function(){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el:'#app-6',
+    data:{
+        message:'Hello Vue!'
+    }
+})
